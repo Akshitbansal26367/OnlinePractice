b5c8c568e09833fa32ef94aaaa102e8beb5bf6bb
Akshit Bansal <110843294+Akshitbansal26367@users.noreply.github.com>
2024-01-05 11:26:57 +0530

    Create Vector-Erase.cpp

---
+++ C++ HackerRank solutions/Vector-Erase.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main() {
+    
+    int n;
+    cin>>n;
+    int element;
+    
+    vector<int> v;
+    
+    for(int i = 0; i<n; i++) {
+        cin >> element;
+        v.push_back(element);
+    }
+    
+    int x;
+    cin>>x;
+    
+    v.erase(v.begin()+(x-1));
+    
+    int a,b;
+    cin >> a >> b;
+    
+    v.erase(v.begin()+(a-1),v.begin()+(b-1));
+    
+    cout << v.size() << endl;
+    for (int i = 0; i<v.size(); i++)
+        cout << v[i] << " ";
+        
+    return 0;
+}
