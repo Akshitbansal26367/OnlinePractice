9dbfd6c7297ab44b9c4ded8aa582ec34648ec480
Akshit Bansal <110843294+Akshitbansal26367@users.noreply.github.com>
2024-03-11 23:52:10 +0530

    Create Reverse a String.java

---
+++ GFG/Reverse a String.java
@@ -0,0 +1,13 @@
+class Reverse
+{
+    // Complete the function
+    // str: input string
+    public static String reverseWord(String str)
+    {
+        String s = "";
+        for (int i = str.length() - 1; i>=0; i--)
+            s = s + str.charAt(i);
+        
+        return s;
+    }
+}
