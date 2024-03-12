4cecd4155c72956c6410568a41e0e3fb94afe4bb
Akshit Bansal <110843294+Akshitbansal26367@users.noreply.github.com>
2024-03-12 22:45:18 +0530

    Update Palindrome String.cpp

--- GFG/Palindrome String.cpp
+++ GFG/Palindrome String.cpp
@@ -23,23 +23,34 @@ public:
 
 
 //C code
-class Solution{
-public:	
+void swap (char *a, char *b) {
+    char temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
-	int isPalindrome(string S)
-	{
-	    int left = 0;
-	    int right = S.size() - 1;
-	    
-	    while (left <= right) {
-	        if (S[left] != S[right])
-	            return 0;
-	       
-	        left++;
-	        right--;
-	    }
-	    
-	    return 1;
-	}
-
-};
+int isPalindrome(char  S[])
+{
+        int i, flag = 1;
+        int size = strlen(S);
+        char temp[size];
+        
+        for (i = 0; i < size; i++)
+            temp[i] = S[i];
+        
+        i = 0;
+        while (i <= size - 1) {
+            swap(&S[size - 1], &S[i]);
+            i++;
+            size--;
+        }
+        
+        for (i = 0; i < size; i++) {
+            if (S[i] != temp[i]) {
+                flag = 0;
+                break;
+            }
+        }
+        
+        return flag;
+}
