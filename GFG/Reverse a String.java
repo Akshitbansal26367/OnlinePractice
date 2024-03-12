ae44def5c7c78c2e0024fc4d939692062204b61e
Akshit Bansal <110843294+Akshitbansal26367@users.noreply.github.com>
2024-03-12 10:20:14 +0530

    Update Reverse a String.java

--- GFG/Reverse a String.java
+++ GFG/Reverse a String.java
@@ -1,3 +1,4 @@
+//Java Code
 class Reverse
 {
     // Complete the function
@@ -11,3 +12,22 @@ class Reverse
         return s;
     }
 }
+
+// C++ code
+class Solution
+{
+    public:
+    string reverseWord(string str)
+    {
+        int i = 0, temp;
+        int size = str.size();
+        
+        while (i <= size-1) {
+            swap (str[size-1],str[i]);
+            i++;
+            size--;
+        }
+        
+        return str;
+    }
+};
