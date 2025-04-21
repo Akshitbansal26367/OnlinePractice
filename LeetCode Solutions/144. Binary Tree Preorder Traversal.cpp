cf183d4697fda7dca0624788250877e22cef9e0b
Akshit Bansal <110843294+Akshitbansal26367@users.noreply.github.com>
2025-04-21 18:54:00 +0530

    Update 144. Binary Tree Preorder Traversal.cpp

--- LeetCode Solutions/144. Binary Tree Preorder Traversal.cpp
+++ LeetCode Solutions/144. Binary Tree Preorder Traversal.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void solve(TreeNode* root, vector<int>& arr) {
+    /*void solve(TreeNode* root, vector<int>& arr) {
         if (root == NULL)
             return;
         arr.push_back(root->val);
@@ -12,5 +12,26 @@ public:
         vector<int> arr;
         solve(root, arr);
         return arr;
+    }*/
+
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> preOrder;
+        if (root == NULL)
+            return preOrder;
+
+        stack<TreeNode*> st;
+        st.push(root);
+
+        while (!st.empty()) {
+            TreeNode* node = st.top();
+            st.pop();
+            preOrder.push_back(node->val);
+
+            if (node->right != NULL)
+                st.push(node->right);
+            if (node->left != NULL)
+                st.push(node->left);
+        }
+        return preOrder;
     }
 };
